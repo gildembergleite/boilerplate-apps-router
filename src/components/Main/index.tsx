d7aeaa211229106b6d678cf38d5433096f10b04c
Gildemberg Leite <gildembergleite@gmail.com>
2023-11-01 09:14:58 -0300

    feat: config and build storybook

--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -1,6 +1,6 @@
-const Main = () => (
+const Main = ({ title = 'React Avançado' }) => (
   <main className="flex w-full py-10 bg-sky-800 justify-center items-center">
-    <h1 className="text-4xl font-bold">React Avançado</h1>
+    <h1 className="text-4xl font-bold">{title}</h1>
   </main>
 )
 
