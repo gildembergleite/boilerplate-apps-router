9d6834363aec1b533a8e9a39d4df478b4d214d83
Gildemberg Leite <gildembergleite@gmail.com>
2023-11-01 09:57:15 -0300

    feat: generate components with plop

--- jest.config.js
+++ jest.config.js
@@ -2,10 +2,14 @@ module.exports = {
   testEnvironment: 'jsdom',
   testPathIgnorePatterns: ['/node_modules/', '/.next/'],
   collectCoverage: true,
-  collectCoverageFrom: ['src/**/*.{ts(x)?}', '!src/app/**'],
+  collectCoverageFrom: [
+    'src/**/*.{ts(x)?}',
+    '!src/app/**',
+    '!/src/components/**/stories.tsx'
+  ],
   setupFilesAfterEnv: ['<rootDir>/.jest/setup.ts'],
   modulePaths: ['<rootDir>/src/'],
   transform: {
-    '^.+\\.(ts|tsx)$': ['babel-jest', { presets: ['next/babel'] }],
-  },
+    '^.+\\.(ts|tsx)$': ['babel-jest', { presets: ['next/babel'] }]
+  }
 }
---
+++ src/components/Button/index.tsx
@@ -0,0 +1,7 @@
+const Button = () => (
+  <main className="flex w-full py-10 bg-sky-800 justify-center items-center">
+    <h1 className="text-4xl font-bold">Button</h1>
+  </main>
+)
+
+export default Button
